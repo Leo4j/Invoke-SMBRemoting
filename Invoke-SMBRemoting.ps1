74d474b28832689256b9d7d5770aaefa471b7203
Rob <61951374+Leo4j@users.noreply.github.com>
2023-10-09 15:36:53 +0100

    Update Invoke-SMBRemoting.ps1

--- Invoke-SMBRemoting.ps1
+++ Invoke-SMBRemoting.ps1
@@ -111,11 +111,7 @@ while (`$true) {
 	
 	$B64ServerScript = [System.Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($ServerScript))
 	
-	$FullCommand = "`$encstring = `"$B64ServerScript`"; `$decodedstring = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String(`$encstring)); Invoke-Expression `$decodedstring"
-	
-	$b64command = [System.Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($FullCommand))
-	
-	$arguments = "\\$ComputerName create $ServiceName binpath= `"C:\Windows\System32\cmd.exe /c powershell.exe -enc $b64command`""
+	$arguments = "\\$ComputerName create $ServiceName binpath= `"C:\Windows\System32\cmd.exe /c powershell.exe -enc $B64ServerScript`""
 	
 	$startarguments = "\\$ComputerName start $ServiceName"
 	
